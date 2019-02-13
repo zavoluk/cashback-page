92a14c566b7d8723641e1a1a70a80c1d8fb90cda
zavoluk <zavoluk@gmail.com>
2019-02-14 02:10:01 +0300

    done

---
+++ source/pages/base/index.js
@@ -0,0 +1,19 @@
+/**
+ * style
+ */
+import '../../styles/reset.sass'
+import '../../styles/global.sass'
+/**
+ * js
+ */
+import '@babel/polyfill'
+import '../../js/matchesPolyfill'
+import '../../js/closestPolyfill'
+import '../../js/onLoad'
+/**
+ * components
+ */
+import '../../components/state-popup'
+import '../../components/header'
+import '../../components/overlay'
+import '../../components/footer'
--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,24 +1,13 @@
 /**
- * style
+ * extends base
  */
-import '../../styles/reset.sass'
-import '../../styles/global.sass'
-/**
- * js
- */
-import '@babel/polyfill'
-import '../../js/matchesPolyfill'
-import '../../js/closestPolyfill'
-import '../../js/onLoad'
+import '../base'
 /**
  * components
  */
-import '../../components/state-popup'
-import '../../components/header'
 import '../../components/banner'
 import '../../components/shops'
 import '../../components/how-it-works'
 import '../../components/about-us'
 import '../../components/feedback'
 import '../../components/text'
-import '../../components/overlay'
