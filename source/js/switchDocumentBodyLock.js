64d7ff84539107aecf9e5107d9ca81e2e612b309
zavoluk <zavoluk@gmail.com>
2019-02-16 22:21:35 +0300

    common fix

--- source/js/switchDocumentBodyLock.js
+++ source/js/switchDocumentBodyLock.js
@@ -1,4 +1,5 @@
-let documentBody = document.body;
+let documentBody = document.body,
+	html = document.querySelector('html');
 
 export default function (_state) {
 	let state = _state.toUpperCase();
@@ -6,9 +7,11 @@ export default function (_state) {
 	switch (state) {
 		case 'LOCK':
 			documentBody.classList.add('no-scroll');
+			html.classList.add('no-scroll');
 			break;
 		case 'UNLOCK':
 			documentBody.classList.remove('no-scroll');
+			html.classList.remove('no-scroll');
 			break;
 		default:
 			throw new Error(`${state} is not valid state`)
